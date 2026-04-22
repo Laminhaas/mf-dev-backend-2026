8743096f6318a983cd70a6162786cb3083cb421f
Matheus Lamas <matheus.galvao.1171871@sga.pucminas.br>
2026-04-21 23:36:40 -0300

    Cadastro de usuario

---
+++ mf-dev-backend-2026/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mf_dev_backend_2026.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nome de usuário é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatório!")]
+        public string Senha { get; set; }
+        [DataType(DataType.Password)]
+
+        [Required(ErrorMessage = "Tipo de perfil é obrigatório!")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
